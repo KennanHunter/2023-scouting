9cda9d46af38bb8dbf53e45baeff726608694357
Kennan Hunter <kennanhunter5@gmail.com>
2023-03-07 23:08:01 -0500

    have fun hazel

--- src/routes/database/CreateQR.tsx
+++ src/routes/database/CreateQR.tsx
@@ -1,79 +1,35 @@
-import { Box, Button, Center, Pagination, Text, Title } from "@mantine/core";
-import { openModal } from "@mantine/modals";
-import { FC, useCallback, useEffect, useState } from "react";
-import QRCode from "react-qr-code";
-import { JSONGzip } from "../../data/formats/JSONGzip";
+import { Box, Button, Center, Text } from "@mantine/core";
+import { FC, useState } from "react";
 import { useMatchDB } from "../../stores/match/matchDB";
-import { MatchState } from "../../stores/match/matchTypes";
 import { usePitDB } from "../../stores/pit/pitDB";
-import { PitState } from "../../stores/pit/pitTypes";
 
 export const CreateQR: FC = () => {
     const pitDB = usePitDB((state) => state.db);
     const matchDB = useMatchDB((state) => state.db);
-    const [fullDB, setFullDB] = useState<(MatchState | PitState)[]>([]);
 
-    useEffect(() => {
-        setFullDB([...pitDB, ...matchDB]);
-    }, [pitDB, matchDB]);
+    const pitNumOfQRCode = Math.ceil(pitDB.length / 10);
+    const matchNumOfQRCode = Math.ceil(matchDB.length / 10);
 
+    const totalNumOfQRCode = pitNumOfQRCode + matchNumOfQRCode;
     const [activePage, setPage] = useState<number | undefined>();
 
-    const openQRCodeModal = useCallback(
-        () =>
-            openModal({
-                title: "Scan QR Codes",
-                centered: true,
-                children: (
-                    <>
-                        <Title>Page {activePage}</Title>
-                        <QRCode
-                            value={(() => {
-                                if (!activePage)
-                                    throw new Error(
-                                        "Active page not initialized in qr code call"
-                                    );
+    const activeSlice = (() => {
+        if (!activePage) return;
 
-                                const activeSlice = pitDB.slice(
-                                    activePage * 10,
-                                    activePage * 10 + 10
-                                );
-
-                                activeSlice.map((val) => {
-                                    const { success: isMatch } =
-                                        MatchState().safeParse(val);
-
-                                    if (isMatch) {
-                                        MatchState().parse(val);
-
-                                        return JSONGzip.match.stringify(val);
-                                    }
-
-                                    if (isMatch) {
-                                        PitState().parse(val);
-
-                                        return JSONGzip.pit.stringify(val);
-                                    }
-                                });
-                            })()}
-                        />
-                        <Pagination total={fullDB.length} />
-                    </>
-                ),
-            }),
-        [activePage]
-    );
-
-    useEffect(() => {
-        if (activePage) openQRCodeModal();
-    }, [activePage]);
+        if (activePage > pitNumOfQRCode) {
+            // do match db stuff
+        }
+        else {
+            // do pit db stuff
+        }
+    })();
 
     return (
         <Box>
             <Center>
                 <Text>
-                    Exporting {fullDB} matches, in
-                    {Math.ceil(fullDB.length / 10)} QR Codes
+                    Exporting {pitDB.length + matchDB.length} matches, in
+                    {totalNumOfQRCode} QR Codes
                 </Text>
                 <Button
                     onClick={() => {
